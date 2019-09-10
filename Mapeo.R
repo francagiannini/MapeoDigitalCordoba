1ba934d57bca8a5162c3935d0085d7547a0fb072
PPacioretti <pablopaccioretti@agro.unc.edu.ar>
2019-09-10 11:10:07 -0300

    Actualizacion

--- Mapeo.R
+++ Mapeo.R
@@ -3,18 +3,20 @@ library(ggplot2)
 library(ggspatial)
 library(ggmap)
 
+limiteProv <- read_sf("Datos/provCba")
 baseDatos <- read.table("Datos/suelos.txt", header = TRUE)
 baseDatos <- st_as_sf(baseDatos, coords = c("Xt","Yt"),  crs = 32720)
 
+
 ggplot(baseDatos) +
   annotation_map_tile(zoom = 10) +
+  geom_sf(data = limiteProv) +
   geom_sf() + 
   annotation_scale(location = "br") + #, width_hint = 0.4) +
   annotation_north_arrow(location = "tl", which_north = "grid", 
                          style = north_arrow_fancy_orienteering) +
   xlab("Longitud") + ylab("Latitud") +
-  theme(panel.grid.major = element_line(color = gray(0.5), linetype = "dashed", size = 0.5))  +
-  coord_sf(crs = 32720)
+  theme(panel.grid.major = element_line(color = gray(0.5), linetype = "dashed", size = 0.5))
 
 
 ggdraw() +
