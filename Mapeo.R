189547da8cc344343c044f3bc14e604d4dbb7deb
PPaccioretti <pablopaccioretti@agro.unc.edu.ar>
2019-09-10 11:02:49 -0300

    Actualizacion

--- Mapeo.R
+++ Mapeo.R
@@ -2,15 +2,14 @@ library(sf)
 library(ggplot2)
 library(ggspatial)
 library(ggmap)
-register_google(key = "[your key]", write = TRUE)
 
 baseDatos <- read.table("Datos/suelos.txt", header = TRUE)
 baseDatos <- st_as_sf(baseDatos, coords = c("Xt","Yt"),  crs = 32720)
 
 ggplot(baseDatos) +
-  annotation_map_tile(zoom = 7) +
+  annotation_map_tile(zoom = 10) +
   geom_sf() + 
-  annotation_scale(location = "br", width_hint = 0.4) +
+  annotation_scale(location = "br") + #, width_hint = 0.4) +
   annotation_north_arrow(location = "tl", which_north = "grid", 
                          style = north_arrow_fancy_orienteering) +
   xlab("Longitud") + ylab("Latitud") +
@@ -18,3 +17,6 @@ ggplot(baseDatos) +
   coord_sf(crs = 32720)
 
 
+ggdraw() +
+  draw_plot(plot_fr) +
+  draw_plot(plot_wolrd, x = 0.15, y = 0.14, width = .25, height = .25)
